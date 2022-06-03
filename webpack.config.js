a24c62937eae0ec68cfa2352ff91ecb43de63e8f
Peter Fields <pcfields@gmail.com>
2022-06-03 16:45:29 +0200

    add webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin =
+    require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
     entry: { index: './src/index.js', print: './src/print.js' },
-    plugins: [new HtmlWebpackPlugin({ title: 'Output Management' })],
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Output Management' }),
+        new BundleAnalyzerPlugin(),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),

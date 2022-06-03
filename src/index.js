ae3ea87189700164dd8e8dec7326bd32de229fc1
Peter Fields <pcfields@gmail.com>
2022-06-03 16:36:35 +0200

    use html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
 import { join } from 'lodash'
-import './styles.css'
-import webpackLogo from './img/webpack-logo.svg'
-
-import Data from './data/data.xml'
-import Notes from './data/data.csv'
-
-import toml from './data/data.toml'
-import yaml from './data/data.yaml'
-import json from './data/data.json5'
+import printMe from './print'
 
 function component() {
     const element = document.createElement('div')
+    const button$ = document.createElement('button')
 
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = join(['Hello', 'webpack'], ' ')
     element.classList.add('hello')
 
-    const logo = new Image()
-    logo.src = webpackLogo
-    element.appendChild(logo)
-
-    console.log({ Data })
-    console.log({ Notes })
-
-    console.log({ toml })
-    console.log('toml title:', toml.title)
-    console.log('toml name:', toml.owner.name)
-
-    console.log({ yaml })
-    console.log('yaml title:', yaml.title)
-    console.log('yaml name:', yaml.owner.name)
-
-    console.log({ json })
-    console.log('json title:', json.title)
-    console.log('json name:', json.owner.name)
+    button$.innerHTML = 'Click me and check the console'
+    button$.onclick = printMe
+    element.appendChild(button$)
 
     return element
 }
 
 document.body.appendChild(component())
-// document.body.appendChild()
